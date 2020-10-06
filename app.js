2fbdd2f6f6370c1887d54c3b3e6e915011d33c2f
e-magon <emagon@emagon.xyz>
2020-10-06 22:01:41 +0200

    First working version

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
-"use strict"
+"use strict";
 
-console.log("ciao");
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+
+// Loads bot settings
+var botSettings = JSON.parse(fs.readFileSync("bot-settings.json"));
+const bot = new TelegramBot(botSettings.token, { polling: true });
+
+bot.onText(/\/chatid/, (msg, match) => {
+  let chatId = msg.chat.id;
+  console.log(`asked chatid in chat ${chatId}, from user ${msg.from.id} (${msg.from.username})`);
+  bot.sendMessage(chatId, chatId);
+});
+
+bot.on("message", async (msg) => {
+  let chatId = msg.chat.id;
+
+  // Check if it is the correct group
+  if (chatId != botSettings.groupid)
+    return;
+
+  // Check if the message is from an admin
+  let status = (await bot.getChatMember(chatId, msg.from.id)).status;
+  if (status == "creator" || status == "administrator" || msg.from.username == "GroupAnonymousBot")
+    return;
+
+  // check if it is an audio msg
+  if (msg.voice) {
+    // check if it is under the limit
+    if (msg.voice.duration <= botSettings.maxaudiosecs)
+      return;
+  } else if (botSettings.allowothermsgs) {
+    // if it isn't an audio message and other msgs are allowed:
+    return;
+  }
+
+  // this will prevent msgs like "msg pinned", "new user" ecc. to be eliminated
+  if (msg.voice || msg.text || msg.audio || msg.document || msg.photo || msg.sticker || msg.video || msg.video_note || msg.contact || msg.poll || msg.location) {
+    bot.deleteMessage(chatId, msg.message_id);
+
+    if (botSettings.senddelmsg)
+      bot.sendMessage(chatId, `Hey ${msg.from.first_name}, invia solo audio inferiori a ${botSettings.maxaudiosecs} secondi!`);
+  }
+});
+
+//console.log(botSettings);
