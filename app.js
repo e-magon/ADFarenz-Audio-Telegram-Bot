2a026d01552572d103fd101a33c22337a25fb90a
e-magon <emagon@emagon.xyz>
2021-02-01 16:19:11 +0100

    Refactors and better comments

--- app.js
+++ app.js
@@ -3,25 +3,29 @@
 const TelegramBot = require("node-telegram-bot-api");
 const fs = require("fs");
 
-// Loads bot settings
+// Load bot settings
 // botmode: 0 (allow everything)    1 (audio msgs only)     2 (text/images msgs only)
 var botSettings = JSON.parse(fs.readFileSync("bot-settings.json"));
 const bot = new TelegramBot(botSettings.token, { polling: true });
 var botUsername;
 
 // set up commands suggestions in Telegram when typing /
+// and get the bot's username
 initialSetUp();
 
-// array of messages that have the settings opened
+// array of messages that have the settings keyboard open
 let openSettingsMessages = [];
 
 // Listen to messages and commands
+// this function is a "validator": if a message is valid, one of the tests performed in it will return and stop the function.
+// if a message isn't valid, the tests will not return and so the message will be deleted at the end of the function
 bot.on("message", async (msg) => {
   // Check if it is the correct group
   if (msg.chat.id != botSettings.groupid) {
     // if it isn't the correct group, the only message it will answer to is chat id
     if (msg.text && msg.text.match(new RegExp(`^\/chatid($|@${botUsername}$)`)))
       cmdChatID(msg);
+    // in any case, it doesn't do anything after
     return;
   }
 
@@ -29,22 +33,25 @@ bot.on("message", async (msg) => {
   let userStatus = (await bot.getChatMember(botSettings.groupid, msg.from.id)).status;
   if (userStatus == "creator" || userStatus == "administrator" || msg.from.username == "GroupAnonymousBot") {
     console.log(`msg from admin ${msg.from.username} (${userStatus})`);
+
     // check if the admin wrote a command
     if (msg.text) {
       let text = msg.text;
       if (text.match(new RegExp(`^\/help($|@${botUsername}$)`))) {
-        
         cmdHelp(msg);
       } else if (text.match(new RegExp(`^\/chatid($|@${botUsername}$)`))) {
         cmdChatID(msg);
       } else if (text.match(new RegExp(`^\/audiobot($|@${botUsername}$)`))) {
-        cmdImpostazioni(msg);
+        cmdSettings(msg);
       }
     }
+
+    // this stops the function and so it prevents the message from being deleted
     return;
   }
 
   // if it isn't from an admin, check if some user tried to do some commands
+  // if he tried to, it immediately deletes the message
   if (msg.text) {
     let text = msg.text;
     if (isACommand(text)) {
@@ -64,12 +71,12 @@ bot.on("message", async (msg) => {
       return;
   }
 
-  // if audio msgs are disallowed, check if the message isn't an audio one
+  // if audio msgs are forbidden, check if the message isn't an audio one
   if (botSettings.botmode == 2 && !msg.voice)
     return;
 
   // delete the message: if it reached this point, every test before were negative, so the message has to be deleted
-  // this if will prevent msgs like "msg pinned", "new user" ecc. to be eliminated
+  // this "if" will prevent msgs like "msg pinned", "new user" ecc. to be eliminated
   if (msg.voice || msg.text || msg.audio || msg.document || msg.photo || msg.sticker || msg.video || msg.video_note || msg.contact || msg.poll || msg.location) {
     if (botSettings.botmode == 1)
       deleteMessage(msg.message_id, `Hey ${msg.from.first_name}, invia solo audio inferiori a ${botSettings.maxaudiosecs} secondi!`);
@@ -78,14 +85,16 @@ bot.on("message", async (msg) => {
   }
 });
 
-// Listen to inline keyboard callbacks
+// Listen to inline keyboard callbacks (the settings buttons)
 bot.on("callback_query", async (pressedButton) => {
+  // if it comes from another chat, ignore the action
   if (pressedButton.message.chat.id != botSettings.groupid)
     return;
 
   // check if the user isn't an admin
   let userStatus = (await bot.getChatMember(botSettings.groupid, pressedButton.from.id)).status;
   if (userStatus != "creator" && userStatus != "administrator" && pressedButton.from.username != "GroupAnonymousBot") {
+    // A normal user tried to edit the settings: the bot will show an alert to the user
     const answerOptions = {
       text: "❌ Non hai i permessi per modificare le impostazioni!",
       show_alert: true
@@ -96,51 +105,66 @@ bot.on("callback_query", async (pressedButton) => {
 
   console.log(pressedButton.data + " --- " + pressedButton.message.message_id + " --- " + pressedButton.message.chat.id);
 
-  // check if the settings msg used is one of the valid ones
+  // check if the settings msg used is one of the valid ones:
+  // if the settings message used for tapping the button isn't in the open settings messages array,
+  // it just closes it (it removes the keyboard)
   if (openSettingsMessages.indexOf(parseInt(pressedButton.message.message_id)) == -1) {
     closeOpenSettingsMessages(pressedButton.message.message_id);
     return;
   }
 
   // if is valid, check what action was performed
-  let editMessage = false;
+  let editMessage = false; // if a setting will change, this will become true
   switch (pressedButton.data) {
+    // the button to allow every message
     case "but_allow_everything":
       if (botSettings.botmode != 0)
         editMessage = true;
       botSettings.botmode = 0;
+      // after pressing a button, all the open settings messages are closed (except for "change audio duration" btn)
       closeOpenSettingsMessages();
       break;
+
+    // the button to allow only audio messages
     case "but_audio_only":
       if (botSettings.botmode != 1)
         editMessage = true;
       botSettings.botmode = 1;
       closeOpenSettingsMessages();
       break;
+
+    // the button to allow only text/media messages
     case "but_chat_only":
       if (botSettings.botmode != 2)
         editMessage = true;
       botSettings.botmode = 2;
       closeOpenSettingsMessages();
       break;
+
+    // the button to show the deletion messages in chat
     case "but_confirm_delete":
       if (botSettings.senddelmsg != true)
         editMessage = true;
       botSettings.senddelmsg = true;
       closeOpenSettingsMessages();
       break;
+
+    // the button to hide the deletion messages in chat
     case "but_hide_delete":
       if (botSettings.senddelmsg != false)
         editMessage = true;
       botSettings.senddelmsg = false;
       closeOpenSettingsMessages();
       break;
+
+    // the button to show the keyboard to change the max audio duration allowed
     case "but_change_duration":
       switchToChangeAudioDuration(pressedButton);
       return;
   }
 
   if (pressedButton.data.startsWith("but_duration_")) {
+    // if the button pressed is one with the new duration of audio msgs, it will substring and take the last 2 chars
     let newDuration = parseInt(pressedButton.data.substring(13, pressedButton.data.length));
     if (botSettings.maxaudiosecs != newDuration) {
       editMessage = true;
@@ -177,6 +201,7 @@ bot.on("callback_query", async (pressedButton) => {
 
 // /help
 async function cmdHelp(msg) {
+  // it dynamically generates the help text from GodFather's command list
   let helpText = "";
   for (let cmd of await bot.getMyCommands()) {
     helpText += `/${cmd.command} - ${cmd.description}\n\n`;
@@ -191,8 +216,8 @@ function cmdChatID(msg) {
 }
 
 // /audiobot
-async function cmdImpostazioni(msg) {
-  // closes old settings msg still open (open: inline keyboard shown)
+async function cmdSettings(msg) {
+  // closes old settings msg still open (open: inline keyboard shown, close: inline keyboard removed)
   closeOpenSettingsMessages();
 
   const options = {
@@ -229,12 +254,14 @@ async function cmdImpostazioni(msg) {
       ]]
     }
   };
+
   let message = await bot.sendMessage(msg.chat.id, generateSettingText(), options);
 
+  // sends the message with the current settings and the inline keyboard to edit them
   openSettingsMessages.push(message.message_id);
 }
 
-// it replaces the normal settings keyboard with one to set the audio duration
+// it replaces the normal settings keyboard with the one that shows the audio durations
 function switchToChangeAudioDuration(pressedButton) {
   const reply_markup = {
     inline_keyboard: [[
@@ -291,19 +318,20 @@ function switchToChangeAudioDuration(pressedButton) {
       }
     ]]
   };
+
   const options = {
     chat_id: botSettings.groupid,
     message_id: pressedButton.message.message_id
   };
 
-  // it edits the keyboard (with an empty one)
+  // it edits the keyboard (with the new one with the avaliable audio durations)
   bot.editMessageReplyMarkup(reply_markup, options);
   return;
 }
 
 // ====================
 
-// deletes a message. If deletion confirmations are shown, it sends the deletion reason and after x seconds it deletes it as well
+// delete a message. If deletion confirmations are shown, it sends the deletion reason and after x seconds it deletes it as well
 async function deleteMessage(msgID, reason) {
   let deleteConfirmationID = null;
   if (botSettings.senddelmsg)
@@ -323,7 +351,7 @@ async function deleteMessage(msgID, reason) {
   return;
 }
 
-// it sends the commands (and descriptions) to BotFather and gets the bot's username
+// send the commands (and descriptions) to BotFather and gets the bot's username
 async function initialSetUp() {
   let commands = [
     {
@@ -336,7 +364,7 @@ async function initialSetUp() {
     },
     {
       command: "audiobot",
-      description: "apre la impostazioni del bot ADFarenz Audio Bot"
+      description: "apre le impostazioni del bot ADFarenz Audio Bot"
     }
   ];
   bot.setMyCommands(commands);
@@ -345,12 +373,12 @@ async function initialSetUp() {
   console.log(botUsername);
 }
 
-// checks if the passed text is a command
+// check if the passed text is a command
 function isACommand(text) {
   return text.match(new RegExp(`^\/(help|chatid|audiobot)($|@${botUsername}$)`));
 }
 
-// it generates the text to be displayed in the configuration message
+// generate the text to be displayed in the configuration message
 function generateSettingText() {
   let textModalita;
   switch (botSettings.botmode) {
@@ -374,7 +402,7 @@ Comunica l'eliminazione dei messaggi: __*${textConfermaElim}*__`;
   return settingsText;
 }
 
-// closes every settings msgs still open in the chat
+// close every settings msgs still open in the chat
 // optional: if messageID is passed, it will only close the specified message
 function closeOpenSettingsMessages(messageID) {
   // if messageID is specified, it closes that message
@@ -403,12 +431,13 @@ function closeOpenSettingsMessages(messageID) {
       message_id: openSettingMsg
     };
 
+    // it edits the keyboard (with an empty one) and removes the message from the open settings messages array
     bot.editMessageReplyMarkup(reply_markup, options);
     openSettingsMessages.splice(k, 1);
   }
 }
 
-// it saves the settings to the json file
+// save the settings to the json file
 function updateSettingsFile() {
   fs.writeFile("bot-settings.json", JSON.stringify(botSettings, null, 2), (err) => {
     if (err)
